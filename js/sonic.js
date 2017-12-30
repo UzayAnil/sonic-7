ecc7fb2d01d20a143cf46c7f9abc908872b1d3f6
Saveliy <s.koles33@gmail.com>
2017-12-31 01:31:08 +0200

    nixuya ne sdelal

--- js/sonic.js
+++ js/sonic.js
@@ -24,12 +24,12 @@ class Sonic {
             context.drawImage(this.img, ...this.runAnimArr[this.animStep], this.x, this.y, this.runAnimArr[this.animStep][2], this.runAnimArr[this.animStep][3]);
             this.animStep = (this.animStep + 1) % this.runAnimArr.length;
         }
-        if(this.state.run && this.state.jump){
+        if (this.state.run && this.state.jump) {
             context.drawImage(this.img, ...this.jumpInRunAnimArr[this.animStep], this.x, this.y, this.jumpInRunAnimArr[this.animStep][2], this.jumpInRunAnimArr[this.animStep][3]);
-            this.animStep = (this.animStep + 1) % this.jumpInRunAnimArr.length; 
+            this.animStep = (this.animStep + 1) % this.jumpInRunAnimArr.length;
         }
-        if(!this.state.run && this.state.jump){
-        	
+        if (!this.state.run && this.state.jump) {
+
         }
         if (!this.state.run && this.state.jump) {
 
@@ -42,29 +42,40 @@ class Sonic {
         this.x += this.speed * this.direction;
     }
 
-    moveY(){
-        if(!this.intersect([ground]))
-           this.y+=gravity;
+    moveY() {
+        if (!this.isIntersecting([ground]))
+            this.y += gravity;
     }
-    intersect(groundArr){
-         for(let groundIndex = 0;groundIndex < groundArr.length; groundIndex++){
+
+    isIntersecting(groundArr) {
+        for (let groundIndex = 0; groundIndex < groundArr.length; groundIndex++) {
             let ground = groundArr[groundIndex];
-            debugger;
-               for(let pointIndex = 0; pointIndex < ground.dotArr.length-1;pointIndex++){
-                 let point1 = ground.dotArr[pointIndex];
-                 let point2 = ground.dotArr[pointIndex+1];
-                 let x = this.x;
-                 let y = this.y+this.h;
-                 let a = dist(new Point(x, y), point1);
-                 let b = dist(new Point(x, y), point2);
-                 let c = dist(point1, point2);
-                 let p = (a + b + c) / 2;
-                 let h = 2 * Math.sqrt(p * (p - a) * (p - b) * (p - c)) / c;
-                 if(h < 2 && this.x >= Math.min(point1.x, point2.x) && this.x <= Math.max(point1.x, point2.x))
+
+            for (let pointIndex = 0; pointIndex < ground.dotArr.length - 1; pointIndex++) {
+                let point1 = ground.dotArr[pointIndex];
+                let point2 = ground.dotArr[pointIndex + 1];
+
+                let isLeft = this.direction === -1;
+                let isUp = point2.y < point1.y;
+                if (isLeft)
+                    isUp = !isUp;
+
+
+                let x = this.x + (isLeft ? this.w : 0);
+                let y = this.y + this.h;
+
+                let a = dist(new Point(x, y), point1);
+                let b = dist(new Point(x, y), point2);
+                let c = dist(point1, point2);
+                let p = (a + b + c) / 2;
+                let h = 2 * Math.sqrt(p * (p - a) * (p - b) * (p - c)) / c;
+
+                if (h < 2 && this.x >= Math.min(point1.x, point2.x) && this.x <= Math.max(point1.x, point2.x)) {
                     return true;
-               }
-         }
-         return false;
+                }
+            }
+        }
+        return false;
     }
 }
 
