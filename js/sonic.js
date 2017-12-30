903ced3c1bcb077618d074fce804c8a52db8bd4f
Saveliy <s.koles33@gmail.com>
2017-12-30 23:23:10 +0200

    Add jump sprites

--- js/sonic.js
+++ js/sonic.js
@@ -9,11 +9,11 @@ class Sonic {
         this.direction = 0;
         this.state = {
             jump: false,
-
             run: false,
         };
         this.animStep = 0;
         this.runAnimArr = [[10, 54, 24, 37], [41, 54, 32, 38], [81, 55, 36, 37], [122, 56, 33, 36], [161, 56, 29, 38], [199, 54, 25, 38], [232, 54, 26, 37], [265, 55, 32, 38], [302, 54, 37, 37], [346, 52, 32, 39], [386, 53, 32, 38], [425, 52, 25, 38]];
+        this.jumpInRunAnimArr = [[10, 102, 24, 37], [49, 102, 36, 34], [101, 102, 25, 38], [140, 101, 25, 38]];
     }
 
     draw() {
@@ -24,13 +24,12 @@ class Sonic {
             context.drawImage(this.img, ...this.runAnimArr[this.animStep], this.x, this.y, this.runAnimArr[this.animStep][2], this.runAnimArr[this.animStep][3]);
             this.animStep = (this.animStep + 1) % this.runAnimArr.length;
         }
-        if(this.state.run && this.state.jump){
-            
-        }
-        if(!this.state.run && this.state.jump){
-        	
-        }
+        if (this.state.run && this.state.jump) {
 
+        }
+        if (!this.state.run && this.state.jump) {
+
+        }
 
 
     }
