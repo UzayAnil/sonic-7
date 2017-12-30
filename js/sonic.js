e5147d4428a53f1626426d9de952a69d4a1882ee
VadimMatyrin <maturin030700@gmail.com>
2017-12-30 23:23:35 +0200

    Sonic jump

--- js/sonic.js
+++ js/sonic.js
@@ -14,6 +14,7 @@ class Sonic {
         };
         this.animStep = 0;
         this.runAnimArr = [[10, 54, 24, 37], [41, 54, 32, 38], [81, 55, 36, 37], [122, 56, 33, 36], [161, 56, 29, 38], [199, 54, 25, 38], [232, 54, 26, 37], [265, 55, 32, 38], [302, 54, 37, 37], [346, 52, 32, 39], [386, 53, 32, 38], [425, 52, 25, 38]];
+        this.jumpInRunAnimArr = [];
     }
 
     draw() {
@@ -25,7 +26,8 @@ class Sonic {
             this.animStep = (this.animStep + 1) % this.runAnimArr.length;
         }
         if(this.state.run && this.state.jump){
-            
+            context.drawImage(this.img, ...this.jumpInRunAnimArr[this.animStep], this.x, this.y, this.jumpInRunAnimArr[this.animStep][2], this.jumpInRunAnimArr[this.animStep][3]);
+            this.animStep = (this.animStep + 1) % this.jumpInRunAnimArr.length; 
         }
         if(!this.state.run && this.state.jump){
         	
--- js/sonicEvents.js
+++ js/sonicEvents.js
@@ -4,14 +4,17 @@ sonicImg.onload = function () {
 
 document.onkeydown = function (event) {
     if (event.keyCode === 39) {
+        sonic.animStep = 0;
         sonic.state.run = true;
         sonic.direction = 1;
     }
     if (event.keyCode === 37) {
+        sonic.animStep = 0;
         sonic.state.run = true;
         sonic.direction = -1;
     }
     if(event.keyCode === 32){
+        sonic.animStep = 0;
     	sonic.state.jump = true;
     }
 
