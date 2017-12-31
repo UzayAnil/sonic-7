97fa26f02ba3630e3987d62b503cac42f41f8363
Saveliy <s.koles33@gmail.com>
2017-12-31 02:30:12 +0200

    Add reverse run

--- js/sonic.js
+++ js/sonic.js
@@ -13,6 +13,7 @@ class Sonic {
         };
         this.animStep = 0;
         this.runAnimArr = [[10, 54, 24, 37], [41, 54, 32, 38], [81, 55, 36, 37], [122, 56, 33, 36], [161, 56, 29, 38], [199, 54, 25, 38], [232, 54, 26, 37], [265, 55, 32, 38], [302, 54, 37, 37], [346, 52, 32, 39], [386, 53, 32, 38], [425, 52, 25, 38]];
+        this.runAnimReverseArr = [[5, 449, 25, 38], [37, 450, 32, 38], [77, 449, 32, 39], [116, 451, 37, 37], [265, 453, 29, 38], [231, 451, 25, 38], [197, 451, 26, 37], [158, 452, 32, 38], [116, 451, 37, 37], [77, 449, 32, 39], [37, 450, 32, 38], [5, 459, 25, 38]];
         this.jumpInRunAnimArr = [[10, 102, 24, 37], [49, 102, 36, 34], [101, 102, 25, 38], [140, 101, 25, 38]];
     }
 
@@ -21,7 +22,11 @@ class Sonic {
             context.drawImage(this.img, 4, 5, this.w, this.h, this.x, this.y, this.w, this.h);
         }
         if (this.state.run && !this.state.jump) {
-            context.drawImage(this.img, ...this.runAnimArr[this.animStep], this.x, this.y, this.runAnimArr[this.animStep][2], this.runAnimArr[this.animStep][3]);
+            if (this.direction === 1)
+                context.drawImage(this.img, ...this.runAnimArr[this.animStep], this.x, this.y, this.runAnimArr[this.animStep][2], this.runAnimArr[this.animStep][3]);
+            else
+                context.drawImage(this.img, ...this.runAnimReverseArr[this.animStep], this.x, this.y, this.runAnimReverseArr[this.animStep][2], this.runAnimReverseArr[this.animStep][3]);
+
             this.animStep = (this.animStep + 1) % this.runAnimArr.length;
         }
         if (this.state.run && this.state.jump) {
@@ -59,15 +64,14 @@ class Sonic {
                 let whichFoot = point1.y < point2.y;
                 let x = this.x + (whichFoot ? 6 : this.w);
                 let y = this.y + this.h;
-                if(x >= Math.min(point1.x, point2.x) - diff && x <= Math.max(point1.x, point2.x) + diff
-                    && y >= Math.min(point1.y, point2.y) - diff && y <= Math.max(point1.y, point2.y) + diff)
-                {
+                if (x >= Math.min(point1.x, point2.x) - diff && x <= Math.max(point1.x, point2.x) + diff
+                    && y >= Math.min(point1.y, point2.y) - diff && y <= Math.max(point1.y, point2.y) + diff) {
                     context.strokeRect(point1.x, point1.y, point2.x - point1.x, point2.y - point1.y);
-                    this.y = ((point2.y - point1.y)*(x - point1.x))/(point2.x - point1.x) + point1.y - this.h;
+                    this.y = ((point2.y - point1.y) * (x - point1.x)) / (point2.x - point1.x) + point1.y - this.h;
                     return true;
                 }
 
-                
+
             }
         }
         return false;
